4a94f0b8b18b36c75cce320ccd38ad7633b339d9
sfechtner <sfechtner42@gmail.com>
2023-11-29 18:13:29 -0800

    Imported starter file

---
+++ Assignment08.py
@@ -0,0 +1,342 @@
+# ------------------------------------------------------------------------------------------------- #
+# Title: Assignment08-Starter files
+# # Description: A collection of classes for managing the application
+# ChangeLog: (Who, When, What)
+# RRoot,1.5.2030,Created Script
+# ------------------------------------------------------------------------------------------------- #
+
+import json
+from datetime import date
+
+# Data -------------------------------------------- #
+FILE_NAME: str = 'EmployeeRatings.json'
+
+MENU: str = '''
+---- Employee Ratings ------------------------------
+  Select from the following menu:
+    1. Show current employee rating data.
+    2. Enter new employee rating data.
+    3. Save data to a file.
+    4. Exit the program.
+--------------------------------------------------
+'''
+
+employees: list = []  # a table of employee data
+menu_choice = ''
+
+
+class Person:
+    """
+    A class representing person data.
+
+    Properties:
+    - first_name (str): The person's first name.
+    - last_name (str): The person's last name.
+
+    ChangeLog:
+    - RRoot, 1.1.2030: Created the class.
+    """
+
+    def __init__(self, first_name: str = "", last_name: str = ""):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @property
+    def first_name(self):
+        return self.__first_name.title()
+
+    @first_name.setter
+    def first_name(self, value: str):
+        if value.isalpha() or value == "":
+            self.__first_name = value
+        else:
+            raise ValueError("The first name should not contain numbers.")
+
+    @property
+    def last_name(self):
+        return self.__last_name.title()
+
+    @last_name.setter
+    def last_name(self, value: str):
+        if value.isalpha() or value == "":
+            self.__last_name = value
+        else:
+            raise ValueError("The last name should not contain numbers.")
+
+    def __str__(self):
+        return f"{self.first_name},{self.last_name}"
+
+
+class Employee(Person):
+    """
+    A class representing employee data.
+
+    Properties:
+    - first_name (str): The employee's first name.
+    - last_name (str): The employee's last name.
+    - review_date (date): The data of the employee review.
+    - review_rating (int): The review rating of the employee's performance (1-5)
+
+    ChangeLog:
+    - RRoot, 1.1.2030: Created the class.
+    """
+
+    def __init__(self, first_name: str = "", last_name: str = "", review_date: str = "1900-01-01", review_rating: int = 3):
+
+        super().__init__(first_name=first_name,last_name=last_name)
+        self.review_date = review_date
+        self.review_rating = review_rating
+
+    @property
+    def review_date(self):
+        return self.__review_date
+
+    @review_date.setter
+    def review_date(self, value: str):
+        try:
+            date.fromisoformat(value)
+            self.__review_date = value
+        except ValueError:
+            raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+
+    @property
+    def review_rating(self):
+        return self.__review_rating
+
+    @review_rating.setter
+    def review_rating(self, value: str):
+        if value in (1, 2, 3, 4, 5):
+            self.__review_rating = value
+        else:
+            raise ValueError("Please choose only values 1 through 5")
+
+    def __str__(self):
+        return f"{self.first_name},{self.last_name},{self.review_date},{self.__review_rating}"
+
+
+class FileProcessor:
+    """
+    A collection of processing layer functions that work with Json files
+
+    ChangeLog: (Who, When, What)
+    RRoot,1.1.2030,Created Class
+    """
+
+    @staticmethod
+    def read_employee_data_from_file(file_name: str, employee_data: list, employee_type: Employee):
+        """ This function reads data from a json file and loads it into a list of dictionary rows
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :param file_name: string data with name of file to read from
+        :param employee_data: list of dictionary rows to be filled with file data
+        :param employee_type: an reference to the Employee class
+        :return: list
+        """
+        try:
+            with open(file_name, "r") as file:
+                list_of_dictionary_data = json.load(file)  # the load function returns a list of dictionary rows.
+                for employee in list_of_dictionary_data:
+                    employee_object = employee_type()
+                    employee_object.first_name=employee["FirstName"]
+                    employee_object.last_name=employee["LastName"]
+                    employee_object.review_date=employee["ReviewDate"]
+                    employee_object.review_rating=employee["ReviewRating"]
+                    employee_data.append(employee_object)
+        except FileNotFoundError:
+            raise FileNotFoundError("Text file must exist before running this script!")
+        except Exception:
+            raise Exception("There was a non-specific error!")
+        return employee_data
+
+    @staticmethod
+    def write_employee_data_to_file(file_name: str, employee_data: list):
+        """ This function writes data to a json file with data from a list of dictionary rows
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :param file_name: string data with name of file to write to
+        :param employee_data: list of dictionary rows to be writen to the file
+
+        :return: None
+        """
+        try:
+            list_of_dictionary_data: list = []
+            for employee in employee_data:  # Convert List of employee objects to list of dictionary rows.
+                employee_json: dict = {"FirstName": employee.first_name,
+                                       "LastName": employee.last_name,
+                                       "ReviewDate": employee.review_date,
+                                       "ReviewRating": employee.review_rating
+                                       }
+                list_of_dictionary_data.append(employee_json)
+
+            with open(file_name, "w") as file:
+                json.dump(list_of_dictionary_data, file)
+        except TypeError:
+            raise TypeError("Please check that the data is a valid JSON format")
+        except PermissionError:
+            raise PermissionError("Please check the data file's read/write permission")
+        except Exception as e:
+            raise Exception("There was a non-specific error!")
+
+
+class IO:
+    """
+    A collection of presentation layer functions that manage user input and output
+
+    ChangeLog: (Who, When, What)
+    RRoot,1.1.2030,Created Class
+    """
+    pass
+
+    @staticmethod
+    def output_error_messages(message: str, error: Exception = None):
+        """ This function displays the a custom error messages to the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.3.2030,Created function
+
+        :param message: string with message data to display
+        :param error: Exception object with technical message to display
+
+        :return: None
+        """
+
+        print(message, end="\n\n")
+        if error is not None:
+            print("-- Technical Error Message -- ")
+            print(error, error.__doc__, type(error), sep='\n')
+
+
+    @staticmethod
+    def output_menu(menu: str):
+        """ This function displays the menu of choices to the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :return: None
+        """
+        print()
+        print(menu)
+        print()
+
+
+    @staticmethod
+    def input_menu_choice():
+        """ This function gets a menu choice from the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :return: string with the users choice
+        """
+        choice = "0"
+        try:
+            choice = input("Enter your menu choice number: ")
+            if choice not in ("1", "2", "3", "4"):  # Note these are strings
+                raise Exception("Please, choose only 1, 2, 3, or 4")
+        except Exception as e:
+            IO.output_error_messages(e.__str__())  # passing the exception object to avoid the technical message
+
+        return choice
+
+
+    @staticmethod
+    def output_employee_data(employee_data: list):
+        """ This function displays employee data to the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :param employee_data: list of employee object data to be displayed
+
+        :return: None
+        """
+        message:str = ''
+        print()
+        print("-" * 50)
+        for employee in employee_data:
+            if employee.review_rating == 5:
+                message = " {} {} is rated as 5 (Leading)"
+            elif employee.review_rating == 4:
+                message = " {} {} is rated as 4 (Strong)"
+            elif employee.review_rating == 3:
+                message = " {} {} is rated as 3 (Solid)"
+            elif employee.review_rating == 2:
+                message = " {} {} is rated as 2 (Building)"
+            elif employee.review_rating == 1:
+                message = " {} {} is rated as 1 (Not Meeting Expectations"
+
+            print(message.format(employee.first_name, employee.last_name, employee.review_date, employee.review_rating))
+        print("-" * 50)
+        print()
+
+
+    @staticmethod
+    def input_employee_data(employee_data: list, employee_type: Employee):
+        """ This function gets the first name, last name, and GPA from the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :param employee_data: list of dictionary rows to be filled with input data
+
+        :return: list
+        """
+
+        try:
+            # Input the data
+            employee_object = employee_type()
+            employee_object.first_name = input("What is the employee's first name? ")
+            employee_object.last_name = input("What is the employee's last name? ")
+            employee_object.review_date = input("What is their review date? ")
+            employee_object.review_rating = int(input("What is their review rating? "))
+            employee_data.append(employee_object)
+
+        except ValueError as e:
+            IO.output_error_messages("That value is not the correct type of data!", e)
+        except Exception as e:
+            IO.output_error_messages("There was a non-specific error!", e)
+
+        return employee_data
+
+
+# Beginning of the main body of this script
+employees = FileProcessor.read_employee_data_from_file(file_name=FILE_NAME,
+                                                       employee_data=employees,
+                                                       employee_type=Employee)  # Note this is the class name (ignore the warning)
+
+# Repeat the follow tasks
+while True:
+    IO.output_menu(menu=MENU)
+
+    menu_choice = IO.input_menu_choice()
+
+    if menu_choice == "1":  # Display current data
+        try:
+            IO.output_employee_data(employee_data=employees)
+        except Exception as e:
+            IO.output_error_messages(e)
+        continue
+
+    elif menu_choice == "2":  # Get new data (and display the change)
+        try:
+            employees = IO.input_employee_data(employee_data=employees, employee_type=Employee)  # Note this is the class name (ignore the warning)
+            IO.output_employee_data(employee_data=employees)
+        except Exception as e:
+            IO.output_error_messages(e)
+        continue
+
+    elif menu_choice == "3":  # Save data in a file
+        try:
+            FileProcessor.write_employee_data_to_file(file_name=FILE_NAME, employee_data=employees)
+            print(f"Data was saved to the {FILE_NAME} file.")
+        except Exception as e:
+            IO.output_error_messages(e)
+        continue
+
+    elif menu_choice == "4":  # End the program
+        break  # out of the while loop
