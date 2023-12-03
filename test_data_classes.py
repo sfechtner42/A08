8ad9f3cee8ec5451d34bec23dbcaf89c0294c61e
sfechtner42 <150216377+sfechtner42@users.noreply.github.com>
2023-12-03 15:51:35 -0800

    Add files via upload

--- test_data_classes.py
+++ test_data_classes.py
@@ -7,16 +7,23 @@
 # ------------------------------------------------------------------------------------------------- #
 
 import unittest
-from processing_classes import Person
+from processing_classes import Person, Employee
 
 
 # Define Classes
 class TestPerson(unittest.TestCase):
     def test_person_init(self):
-        person=Person('Sabrina', 'Fechtner')
+        person=Person('sabrina', 'fechtner')
         self.assertEqual("Sabrina", person.first_name)
         self.assertEqual('Fechtner', person.last_name)
 
+class TestEmployee(unittest.TestCase):
+    def test_employee_init(self):
+        employee = Employee('sabrina', 'fechtner','2000-01-01',5)
+        self.assertEqual("Sabrina", employee.first_name)
+        self.assertEqual('Fechtner', employee.last_name)
+        self.assertEqual('2000-01-01', employee.review_date)
+        self.assertEqual(5, employee.review_rating)
 
 
 if __name__ == "__main__":
